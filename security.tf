4db44ac3e9973c1f8d6293d527040289e54fddc1
bransynluther <bransynluther@gmail.com>
2018-12-04 09:13:38 -0500

    Update security to include http on port 80

--- security.tf
+++ security.tf
@@ -9,10 +9,22 @@ ingress {
       "0.0.0.0/0"
     ]
 
-from_port = 22
+  from_port = 22
     to_port = 22
     protocol = "tcp"
-  }
+
+}
+
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+
+  from_port = 80
+    to_port = 80
+    protocol = "tcp"
+}
+
 
 // Terraform removes the default rule
   egress {
@@ -21,5 +33,6 @@ from_port = 22
    protocol = "-1"
    cidr_blocks = ["0.0.0.0/0"]
  }
-
 }
+
+
